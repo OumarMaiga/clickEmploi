a866578982cf952b0c63434fae1f0ae4826b0ee6
Hasseye <oumarm611@gmail.com>
2022-04-13 23:05:16 +0000

    Redesign de profil

--- resources/views/pages/profil.blade.php
+++ resources/views/pages/profil.blade.php
@@ -1,10 +1,10 @@
 <x-app-layout>
-    <div class="main-content">
+    <div class="profil-head">
         <div class="row">
-            <div class="col-md-4 d-flex justify-content-center">
-                <img alt="profil" src="{{$photo}}" class="profil-img"/>
+            <div class="col-md-2 d-flex justify-content-center">
+                <img alt="" src="{{$photo}}" class="profil-img"/>
             </div>
-            <div class="col-md-8">
+            <div class="col-md-10">
                 <!-- Session Status -->
                 <x-auth-session-status class="mb-4" :status="session('status')" />
 
@@ -12,52 +12,64 @@
                     {{ $user->prenom." ".$user->nom }}
                     @if ($user->email == Auth::user()->email)
                         <a href="{{ route('edit_profil', $user->email) }}" class="align-items-center"> 
-                            <button class="btn-modif-profil btn btn-outline-warning">Modifier le profil</button>
+                            <button class="btn-modif-profil btn btn-outline-custom">Modifier le profil</button>
                         </a>   
                     @endif
                 </div>
-                <div class="profil-email">
-                    {{ $user->email }}
+                <div class="profil-description">
+                    {{ $user->adresse }}
                 </div>
                 <div class="profil-description">
-                    Tel: {{ $user->telephone }}
-                </div>
-                <div class="profil-description">
-                    Adresse: {{ $user->adresse }}
-                </div>
-                @if ($user->type == "user")
-                    <div class="profil-description">
-                        Secteur d'activité: {{ $activites->implode(', ') }}
-                    </div>
-                @endif
-                <div class="profil-description">
-                    Curiculium Vitea: 
-                    @if (voir_cv_profil($user->id) == false)
-                        CV non uploader
-                    @else
-                        <a target="_blank" href="{{ voir_cv_profil($user->id)->file_path }}" class="btn-link">
-                            <u>Voir le CV</u>
-                        </a> 
-                    @endif 
-                </div>  
-                <br/>
-                <hr/>
-                <br/>
-                <div class="row">
-                    <div class="col text-center">
-                        <h6 class="subtitle">Emploi publié</h6>
-                        <p class="number">2</p>
-                    </div>
-                    <div class="col text-center">
-                        <h6 class="subtitle">Formation publiée</h6>
-                        <p class="number">1</p>
-                    </div>
-                    <div class="col text-center">
-                        <h6 class="subtitle">Stage publié</h6>
-                        <p class="number">5</p>
-                    </div>
-                </div>
+                    {{ $user->telephone }}
+                </div> 
             </div>
+        </div>
+    </div>
+    <div class="profil-body">
+        @if ($user->type == "user")
+        <div class="row mb-4">
+            <div class="col-sm-3">
+                Email
+            </div> 
+            <div class="col-sm-9 profil-description">
+                {{ $user->email }}
+            </div> 
+        </div>
+        @endif
+        <div class="row mb-4">
+            <div class="col-sm-3">
+                Secteur d'activité
+            </div> 
+            <div class="col-sm-9 profil-description">
+                {{ $activites->implode(', ') }}
+            </div> 
+        </div>
+        <div class="row mb-4">
+            <div class="col-sm-3">
+                Adresse
+            </div> 
+            <div class="col-sm-9 profil-description">
+                {{ $user->adresse }}
+            </div> 
+        </div>
+        <div class="row mb-4">
+            <div class="col-sm-3">
+                Telephone
+            </div> 
+            <div class="col-sm-9 profil-description">
+                {{ $user->telephone }}
+            </div> 
+        </div>
+        <div class="row mb-4">
+            <div class="col-sm-12">
+                @if (voir_cv_profil($user->id) == false)
+                    Pas de CV
+                @else
+                    <a target="_blank" href="{{ voir_cv_profil($user->id)->file_path }}" class="btn btn-custom">
+                        <u>Voir le CV</u>
+                    </a> 
+                @endif 
+            </div> 
         </div>
     </div>
 </x-app-layout>
