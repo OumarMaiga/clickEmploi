1371b612b6e112b94a79f85054ea69acc56ecb4b
Hasseye <oumarm611@gmail.com>
2022-05-14 21:55:08 +0000

    mail

--- resources/views/mail.blade.php
+++ resources/views/mail.blade.php
@@ -1,4 +1,4 @@
-<h3>Bonjour, {{ $title }}</h3>
+<h3>Bonjour, {{ $entreprise_name }}</h3>
 <div class="">
     Bonjour, Nous <a href="https://clickemploi.com">clickemploi.com</a> venons par la présente vous recommander la candidature de {{ $user_name }} pour le poste de {{ $title}} que nous estimons avoir les aptitudes nécessaires pour la bonne exécution de tâches afférentes à ce poste.<br/>
     Vous trouverez les coordonnées les détails sur son niveau de comptabilité entre son profil et ledit poste ainsi que son curriculum vitae (CV) en pièces jointes.<br/> <br/> 
